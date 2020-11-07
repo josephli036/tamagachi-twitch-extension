2d6592444ab07c800f60dee11f61a8f6804f1b13
Ricky Thonglyvong <rickythonglyvong@MacBook-Pro.attlocal.net>
2020-11-07 14:21:06 -0800

    added time stamp for combatConstants

--- twitch-tamagachi-backend/scripts/combatConstants.sql
+++ twitch-tamagachi-backend/scripts/combatConstants.sql
@@ -2,6 +2,7 @@
 DROP TABLE combatConstants;
 CREATE TABLE combatConstants (
     channel_id VARCHAR(30),
+    last_updated TIMESTAMP default CURRENT_TIMESTAMP not null,
     attack_modifier BIGINT,
     jump_modifier BIGINT,
     shield_modifier BIGINT,

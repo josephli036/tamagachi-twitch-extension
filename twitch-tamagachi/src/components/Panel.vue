c1f6da7bc5cbf258d70bc5addf96c0c5500dbcc8
josephlitribe <joseph@tribedynamics.com>
2020-11-07 14:00:56 -0800

    add starting template

--- twitch-tamagachi/src/components/Panel.vue
+++ twitch-tamagachi/src/components/Panel.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Panel</div>
+  <v-card height=500>
+    <v-container>
+      <v-row>
+        <v-col cols=5 offset=7>
+          upgrades
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
 <script>

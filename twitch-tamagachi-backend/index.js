0c5a04816dbb7e452eb5863b624c1fb35937832b
Andrew Thongsavath <andrew@athongsavath.com>
2020-11-07 00:19:39 -0600

    Add database

--- twitch-tamagachi-backend/index.js
+++ twitch-tamagachi-backend/index.js
@@ -1,9 +1,39 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const { Pool, Client } = require('pg')
+require('dotenv').config()
+var jwt = require('jsonwebtoken');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// var connection = pg.createConnection({
+//     host     : process.env.RDS_HOSTNAME,
+//     user     : process.env.RDS_USERNAME,
+//     password : process.env.RDS_PASSWORD,
+//     port     : process.env.RDS_PORT
+//   });
+
+const app = express();
+const port = 3000;
+
+
+
+
+app.get('/', async (req, res) => {
+    try {
+        const client = new Client({
+                host     : process.env.RDS_HOSTNAME,
+                user     : process.env.RDS_USERNAME,
+                password : process.env.RDS_PASSWORD,
+                database : process.env.RDS_PGDB,
+                port     : process.env.RDS_PORT
+              })
+        await client.connect()
+        const result = await client.query('SELECT NOW()')
+        console.log(result)
+        await client.end()
+        await res.send('Hello World!')
+
+    } catch (e) {
+        console.log("caught error: ", e.message)
+    }
 })
 
 app.listen(port, () => {

698480c103d10024c47eceae09d6592ddb5438bf
josephlitribe <joseph@tribedynamics.com>
2020-11-07 16:17:06 -0800

    Add upgrade buttons and attribute buttons

---
+++ twitch-tamagachi/src/components/UpgradeOptions.vue
@@ -0,0 +1,36 @@
+<template>
+  <attribute-selection-menu v-if="!showUpgradeOptions" @attributeClick="onAttributeClick"/>
+  <upgrade-selection-menu v-else @upgradeClick="onUpgradeClick"/>
+</template>
+
+<script>
+import AttributeSelectionMenu from "../components/AttributeSelectionMenu.vue";
+import UpgradeSelectionMenu from "../components/UpgradeSelectionMenu.vue";
+
+export default {
+  name: "UpgradeOptions",
+  components: {AttributeSelectionMenu, UpgradeSelectionMenu},
+  mixins: [],
+  data() {
+    return {
+      currentAttribute: null,
+    };
+  },
+  computed: {
+    showUpgradeOptions() {
+      return this.currentAttribute ? true : false;
+    },
+  },
+  mounted() {},
+  methods: {
+    onAttributeClick(type) {
+      this.currentAttribute = type;
+    },
+    onUpgradeClick(type) {
+      if (type === 'back') {
+        this.currentAttribute = null;
+      }
+    },
+  },
+};
+</script>

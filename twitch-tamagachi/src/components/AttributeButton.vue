18ac5980aee3d73e3627dfc64a6d5add0dfa6d8d
Trent <nguy2284@umn.edu>
2020-11-08 01:08:13 -0600

    custom speed dial overlay

---
+++ twitch-tamagachi/src/components/AttributeButton.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-fab-transition>
+    <v-speed-dial
+      direction="left"
+      transition="slide-x-reverse-transition"
+      style="padding-bottom: 4px"
+    >
+      <template v-slot:activator>
+        <v-btn
+          :color="color"
+          dark
+          fab
+          small
+          depressed
+        >
+          <v-icon>{{ icon }}</v-icon>
+        </v-btn>
+      </template>
+      <v-btn
+        :color="color"
+        dark
+        fab
+        small
+        depressed
+      >
+        <v-icon>mdi-numeric-5</v-icon>
+      </v-btn>
+      <v-btn
+        :color="color"
+        dark
+        fab
+        small
+        depressed
+      >
+        <v-icon>mdi-numeric-3</v-icon>
+      </v-btn>
+      <v-btn
+        :color="color"
+        dark
+        fab
+        small
+        depressed
+      >
+        <v-icon>mdi-numeric-1</v-icon>
+      </v-btn>
+    </v-speed-dial>
+  </v-fab-transition>
+</template>
+
+<script>
+  export default {
+    name: "AttributeButton",
+    props: {
+      color: {
+        type: String,
+        required: true
+      },
+      icon: {
+        type: String,
+        required: true
+      }
+    },
+
+    data: () => ({
+      transition: 'slide-y-reverse-transition',
+    }),
+
+    computed: {
+    },
+
+    watch: {
+    },
+  }
+</script>

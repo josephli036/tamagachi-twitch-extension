b9efbd7a50a160c56945001bf7457a6f15189d52
Ricky Thonglyvong <rthonglyvong@gmail.com>
2020-11-08 02:58:36 -0600

    added stat banner

--- twitch-tamagachi/src/components/Panel.vue
+++ twitch-tamagachi/src/components/Panel.vue
@@ -1,12 +1,20 @@
 <template>
   <v-card class="pa-0" height="500" width="300">
+    <v-row>
+      <stat-banner
+        :attack="attack"
+        :shield="shield"
+        :focus="focus"
+        :jump="jump"
+      />
+    </v-row>
     <v-overlay absolute opacity="0">
       <div class="d-flex fill-height" style="flex-direction: column">
         <v-spacer></v-spacer>
         <div class="d-flex">
           <v-spacer></v-spacer>
           <div class="pa-0">
-            <upgrade
+            <upgrade-button
               @upgrade="onUpgrade"
             />
           </div>
@@ -18,21 +26,29 @@
 
 <script>
 import BackendApi from "../services/backend.js";
-// import StatBanner from "../components/StatBanner.vue";
+import StatBanner from "../components/StatBanner.vue";
 import UpgradeButton from "../components/UpgradeButton.vue";
 
 export default {
   name: "Panel",
   components: {
     UpgradeButton,
-    // StatBanner,
+    StatBanner,
   },
   mixins: [],
   data() {
-    return {};
+    return {
+      attack: 0,
+      shield: 0,
+      focus: 0,
+      jump: 0,
+      spend: 0,
+    };
   },
   computed: {},
-  mounted() {},
+  mounted() {
+    this.updatePoints();
+  },
   methods: {
     onUpgrade({ attribute, type }) {
       BackendApi.upgrade(
@@ -42,22 +58,32 @@ export default {
         type,
         window.authToken
       ).then((data) => {
+        this.attack = data.attack_stat;
+        this.shield = data.shield_stat;
+        this.focus = data.focus_stat;
+        this.jump = data.jump_stat;
         console.log(data);
       });
     },
 
     updatePoints() {
-      setInterval(() => {
-        BackendApi.updatePoints(
-          window.Twitch.ext.viewer.id,
-          window.channelId,
-          window.authToken
-        );
-      }, 30000);
+      BackendApi.updatePoints(
+        window.Twitch.ext.viewer.id,
+        window.channelId,
+        window.authToken
+      ).then(data => {
+        this.attack = data.attack_stat;
+        this.shield = data.shield_stat;
+        this.focus = data.focus_stat;
+        this.jump = data.jump_stat;
+        console.log(data);
+      });
     },
   },
   created() {
-    this.updatePoints();
+    setInterval(() => {
+      this.updatePoints();
+    }, 30000);
   },
 };
 </script>

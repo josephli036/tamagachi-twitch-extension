d00c8b0117f3731fd167144763597065377116f9
Andrew Thongsavath <andrew@athongsavath.com>
2020-11-07 19:29:01 -0600

    Update controllers to use user_id instead of opaque_user_id

--- twitch-tamagachi-backend/controllers/hiscores.js
+++ twitch-tamagachi-backend/controllers/hiscores.js
@@ -4,26 +4,25 @@ const { client } = require('../client')
 hiscoresRouter.get('/', async (req,res) => {
     /*
      * Returns the top 10 players from the leaderboards AND the requesting player's score
-     * The requesting player's opaque_user_id is stored in res.locals
-     * @return [{channel_id, opaque_user_id, last_updated, total_points, attack_stat, jump_stat, shield_stat, focus_stat, player_rank},...{}] is of length 0..11 inclusive
+     * The requesting player's user_id is stored in res.locals
+     * @return [{channel_id, user_id, last_updated, total_points, attack_stat, jump_stat, shield_stat, focus_stat, player_rank},...{}] is of length 0..11 inclusive
      * 
      * NOTE: If the length is 11, then that means there may be a large gap between the lowest rank and 10
     */
-    const currentUserId = res.locals.token.opaque_user_id
-    const hiscorePlayersQuery = 'SELECT channel_id, opaque_user_id, last_updated, total_points, attack_stat, jump_stat, shield_stat, focus_stat, RANK() OVER(ORDER BY total_points DESC) player_rank FROM players ORDER BY total_points DESC LIMIT 10'
-    // const hiscorePlayersQuery = 'SELECT *, RANK() OVER(ORDER BY total_points DESC) user_rank FROM players ORDER BY points DESC LIMIT 10'
+    const currentUserId = res.locals.token.user_id
+    const hiscorePlayersQuery = 'SELECT channel_id, user_id, last_updated, total_points, attack_stat, jump_stat, shield_stat, focus_stat, RANK() OVER(ORDER BY total_points DESC) player_rank FROM players ORDER BY total_points DESC LIMIT 10'
     const hiscorePlayers = (await client.query(hiscorePlayersQuery)).rows
 
     let missingPlayer = true
     for (const hiscorePlayer of hiscorePlayers) {
-        if (hiscorePlayer.opaque_user_id === currentUserId) {
+        if (hiscorePlayer.user_id === currentUserId) {
             missingPlayer = false
             break;
         }
     }
 
     if (missingPlayer) {
-        const singleScoreQuery = 'SELECT * FROM (SELECT channel_id, opaque_user_id, last_updated, total_points, attack_stat, jump_stat, shield_stat, focus_stat, RANK() OVER(ORDER BY total_points DESC) user_rank FROM players) o WHERE o.opaque_user_id = $1'
+        const singleScoreQuery = 'SELECT * FROM (SELECT channel_id, user_id, last_updated, total_points, attack_stat, jump_stat, shield_stat, focus_stat, RANK() OVER(ORDER BY total_points DESC) user_rank FROM players) o WHERE o.user_id = $1'
         const singleScore = await client.query(singleScoreQuery, [currentUserId])
 
         hiscorePlayers.push(singleScore.rows[0])
--- twitch-tamagachi-backend/controllers/players.js
+++ twitch-tamagachi-backend/controllers/players.js
@@ -10,20 +10,18 @@ playersRouter.post('/', async (req,res) => {
     const currentOpaqueUserId = res.locals.token.opaque_user_id
     const currentUserId = res.locals.token.user_id
     const channelId = res.locals.token.channel_id
-    const playerQuery = 'SELECT * FROM players WHERE opaque_user_id = $1 and channel_id = $2'
-    let player = (await client.query(playerQuery, [currentOpaqueUserId, channelId])).rows
+    const playerQuery = 'SELECT * FROM players WHERE user_id = $1 and channel_id = $2'
+    let player = (await client.query(playerQuery, [currentUserId, channelId])).rows
     
     if (player.length === 0) {
-        console.log("in here?")
         const currentTime = new Date()
-        const newPlayer = {channel_id: channelId, opaque_user_id: currentOpaqueUserId, last_updated: currentTime, total_points: 1, points_to_spend:1, attack_stat:0, jump_stat:0, shield_stat:0, focus_stat:0}
+        const newPlayer = {channel_id: channelId, user_id: currentUserId, opaque_user_id: currentOpaqueUserId, last_updated: currentTime, total_points: 1, points_to_spend:1, attack_stat:0, jump_stat:0, shield_stat:0, focus_stat:0}
         const insertFirstScoreQuery = 'INSERT INTO players WHERE (channel_id, user_id, opaque_user_id, last_updated, total_points, points_to_spend, attack_stat, jump_stat, shield_stat, focus_stat) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);'
         await client.query(insertFirstScoreQuery, [channelId, currentUserId, currentOpaqueUserId, currentTime, 1, 1, 0, 0, 0, 0])
 
         return res.status(200).json(newPlayer)
     } else {
         player = player[0]
-        delete player.user_id
 
         const MINUTE = 60 * 1000
         let lastUpdated = player.last_updated
@@ -36,8 +34,8 @@ playersRouter.post('/', async (req,res) => {
         const currentTime = new Date()
 
         if (currentTime >= pointThresholdTime) {
-            const updatePlayerQuery = 'UPDATE players SET total_points = $1, points_to_spend =$2 WHERE opaque_user_id = $3 and channel_id = $4'
-            await client.query(updatePlayerQuery, [Number(player.total_points)+1, Number(player.points_to_spend)+1, currentOpaqueUserId, channelId])
+            const updatePlayerQuery = 'UPDATE players SET total_points = $1, points_to_spend =$2 WHERE user_id = $3 and channel_id = $4'
+            await client.query(updatePlayerQuery, [Number(player.total_points)+1, Number(player.points_to_spend)+1, currentUserId, channelId])
             const updatedPlayer = {...player, last_updated: currentTime, total_points:Number(player.total_points)+1, points_to_spend:Number(player.points_to_spend)+1}
             return res.status(200).json(updatedPlayer)
         }

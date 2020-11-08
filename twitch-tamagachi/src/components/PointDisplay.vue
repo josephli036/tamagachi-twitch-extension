b6ebc966f20af7f4e39ce17bda634d9fd7d580f1
josephlitribe <joseph@tribedynamics.com>
2020-11-08 11:15:12 -0800

    meh

--- twitch-tamagachi/src/components/PointDisplay.vue
+++ twitch-tamagachi/src/components/PointDisplay.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app-bar rounded dense dark color="grey darken-1">
+  <v-app-bar rounded dense dark color="#6441A4">
     <v-spacer />
     <v-icon>mdi-currency-php</v-icon>
     <v-toolbar-title class="text-h5">{{ points }}</v-toolbar-title>

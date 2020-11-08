b5fb7f3145f7c71dcf8d77a84a167d34afe28eba
Andrew Thongsavath <andrew@athongsavath.com>
2020-11-07 18:27:48 -0600

    clean up

--- twitch-tamagachi-backend/controllers/players.js
+++ twitch-tamagachi-backend/controllers/players.js
@@ -4,25 +4,29 @@ const { client } = require('../client')
 playersRouter.post('/', async (req,res) => {
     /*
      * Updates the points of the player, IF an appropriate amount of time has passed
-     * @return {channel_id, opaque_user_id, last_updated, points_to_spend}
+     * @return {channel_id, opaque_user_id, last_updated, total_points, points_to_spend, attack_stat, jump_stat, shield_stat, focus_stat}
     */ 
 
     const currentOpaqueUserId = res.locals.token.opaque_user_id
-    const currentOpaqueUserId = res.locals.token.user_id
+    const currentUserId = res.locals.token.user_id
     const channelId = res.locals.token.channel_id
-    const playerQuery = 'SELECT * FROM playerse WHERE opaque_user_id = $1 and channel_id = $2'
-    const player = (await client.query(playerQuery, [currentOpaqueUserId, channelId])).rows
+    const playerQuery = 'SELECT * FROM players WHERE opaque_user_id = $1 and channel_id = $2'
+    let player = (await client.query(playerQuery, [currentOpaqueUserId, channelId])).rows
     
     if (player.length === 0) {
+        console.log("in here?")
         const currentTime = new Date()
-        const newPlayer = {channel_id: channelId, opaque_user_id: currentOpaqueUserId, last_updated: currentTime, points_to_spend:1}
-        const insertFirstScoreQuery = 'INSERT INTO players WHERE (channel_id, user_id, opaque_user_id, last_updated, points) VALUES ($1, $2, $3, $4, $5, $6);'
-        await client.query(insertFirstScoreQuery, [channelId, currentUserId, currentOpaqueUserId, currentTime, 1])
+        const newPlayer = {channel_id: channelId, opaque_user_id: currentOpaqueUserId, last_updated: currentTime, total_points: 1, points_to_spend:1, attack_stat:0, jump_stat:0, shield_stat:0, focus_stat:0}
+        const insertFirstScoreQuery = 'INSERT INTO players WHERE (channel_id, user_id, opaque_user_id, last_updated, total_points, points_to_spend, attack_stat, jump_stat, shield_stat, focus_stat) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);'
+        await client.query(insertFirstScoreQuery, [channelId, currentUserId, currentOpaqueUserId, currentTime, 1, 1, 0, 0, 0, 0])
+
         return res.status(200).json(newPlayer)
     } else {
-        const MINUTE = 60 * 1000
+        player = player[0]
+        delete player.user_id
 
-        let lastUpdated = player[0].last_updated
+        const MINUTE = 60 * 1000
+        let lastUpdated = player.last_updated
 
         // Round last_updated time up to the nearest minute
         let pointThresholdTime = new Date(lastUpdated.getTime() + MINUTE)
@@ -32,9 +36,9 @@ playersRouter.post('/', async (req,res) => {
         const currentTime = new Date()
 
         if (currentTime >= pointThresholdTime) {
-            const updatePlayerQuery = 'UPDATE players SET points = $1 WHERE opaque_user_id = $2 and channel_id = $3'
-            await client.query(updatePlayerQuery, [player.points+1, currentOpaqueUserId, channelId])
-            const updatedPlayer = {...player, last_updated: currentTime, points:player.points+1}
+            const updatePlayerQuery = 'UPDATE players SET total_points = $1, points_to_spend =$2 WHERE opaque_user_id = $3 and channel_id = $4'
+            await client.query(updatePlayerQuery, [Number(player.total_points)+1, Number(player.points_to_spend)+1, currentOpaqueUserId, channelId])
+            const updatedPlayer = {...player, last_updated: currentTime, total_points:Number(player.total_points)+1, points_to_spend:Number(player.points_to_spend)+1}
             return res.status(200).json(updatedPlayer)
         }
 
